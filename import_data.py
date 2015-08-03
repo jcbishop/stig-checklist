e1c926931aaeb336e3e2db9f3bbe2fe102f84dd1
James Bishop <james.bishop@otechresources.com>
2015-08-02 23:38:53 -0400

    fixed import of data; added stig_checklist.p

--- import_data.py
+++ import_data.py
@@ -21,7 +21,7 @@ def import_stig(xlsfile):
         curr_row = -1
         while curr_row < num_rows:
             curr_row += 1
-            if curr_row < 8:
+            if curr_row < 7:
                 continue
             row = str(wksht.row(curr_row))
             num_cols = wksht.ncols
@@ -73,8 +73,78 @@ def import_stig(xlsfile):
     conn.commit()
 
 
+def import_scan(xlsfile):
+    c = conn.cursor()
+    # Create table
+    try:
+        c.execute('''CREATE TABLE scan_results (plugin_id text, cve text, cvss text, risk text, host text, protocol text, port text, name text, synopsis text, description text, solution text, see_also text, plugin_output text)''')
+    except Exception as e:
+        print e
+    fpath = os.path.curdir
+    file = os.path.join(fpath,xlsfile)
+    workbook = xlrd.open_workbook(file,logfile=sys.stdout)
+    worksheets = workbook.sheet_names()
+    for worksheetname in worksheets:
+        wksht = workbook.sheet_by_name(worksheetname)
+        print wksht
+        num_rows = wksht.nrows - 1
+        curr_row = -1
+        while curr_row < num_rows:
+            curr_row += 1
+            if curr_row < 2:
+                continue
+            row = str(wksht.row(curr_row))
+            num_cols = wksht.ncols
+            #Plugin ID	CVE	CVSS	Risk	Host	Protocol	Port	Name	Synopsis	Description	Solution	See Also	Plugin Output
+            plugin_id_v = str(wksht.cell_value(curr_row,0))
+            cve_v = str(wksht.cell_value(curr_row,1))
+            try:
+                cvss_v = str(wksht.cell_value(curr_row,2))
+            except UnicodeEncodeError as ue:
+                print ue
+                cvss_v = (wksht.cell_value(curr_row,2))
+            risk_v = str(wksht.cell_value(curr_row,3))
+            host_v = str(wksht.cell_value(curr_row,4))
+            try:
+                protocol_v = str(wksht.cell_value(curr_row,5))
+            except UnicodeEncodeError as ue:
+                print ue
+                protocol_v = (wksht.cell_value(curr_row,5))
+            port_v = str(wksht.cell_value(curr_row,6))
+            name_v = str(wksht.cell_value(curr_row,7))
+            try:
+                synopsis_v = str(wksht.cell_value(curr_row,8))
+            except UnicodeEncodeError as ue:
+                print ue
+                synopsis_v = (wksht.cell_value(curr_row,8))
+            try:
+                description_v = str(wksht.cell_value(curr_row,9))
+            except UnicodeEncodeError as ue:
+                print ue
+                description_v = (wksht.cell_value(curr_row,9))
+            try:
+                solution_v = str(wksht.cell_value(curr_row,10))
+            except UnicodeEncodeError as ue:
+                print ue
+                solution_v = (wksht.cell_value(curr_row,10))
+            see_also_v = str(wksht.cell_value(curr_row,11))
+            plugin_output_v = str(wksht.cell_value(curr_row,12))
+
+            print "Inserting row %d" % curr_row
+            try:
+                c.execute('''INSERT INTO scan_results(plugin_id, cve, cvss, risk, host, protocol, port, name, synopsis, description, solution, see_also, plugin_output) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,? )''', (plugin_id_v, cve_v, cvss_v, risk_v, host_v, protocol_v, port_v, name_v, synopsis_v, description_v, solution_v, see_also_v, plugin_output_v))
+            except UnicodeEncodeError as ue:
+                print ue
+                print "failed to insert row"
+                print row
+
+    conn.commit()
+
+
 def main():
     import_stig('RHEL5_STIG_V1R10-32bit.xlsx')
+    import_scan('RHEL5_172_22_83_165_32-bit_cc3clv_Jul_9_2015.xlsx')
+
 
 if __name__ == "__main__":
     main()
--- stig_checklist.py
+++ stig_checklist.py
@@ -1,4 +1,44 @@
 __author__ = 'jbishop'
 import sqlite3
-nessus_db = sqlite3.connect('nessus_results.db')
 stig_db = sqlite3.connect('stig_checklist.db')
+
+
+def iterate_scan_res():
+
+    c = stig_db.execute("SELECT stig_id from checklist")
+    chklist_id = c.fetchall()
+    for id in chklist_id:
+        x = id[0]
+        try:
+            s = stig_db.execute("SELECT description from scan_results where description LIKE '\"" + x + "%'")
+        except Exception as e:
+            print e
+            return -1
+        try:
+            scan_res = s.fetchall()
+        except Exception as e:
+            print e
+            return -1
+        if [[ scan_res is not None ]]:
+            num_rows = scan_res.__len__()
+            if num_rows >= 2:
+                print num_rows
+                for row in scan_res:
+                    description = row
+                    print description
+            else:
+                if [[ "[PASSED]" in scan_res ]]:
+                    print "%s: PASSED" % id[0]
+                elif [[ "[FAILED]" in scan_res ]]:
+                    print "%s: FAILED" % id[0]
+                else:
+                    print "%s: Warning" % id[0]
+        else:
+            continue
+    return
+
+def main():
+    iterate_scan_res()
+
+if __name__ == "__main__":
+    main()
